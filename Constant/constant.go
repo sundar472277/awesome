6da91847d9a30dc5ac8abac9ae76732bccf52bde
RajiMunichamy1993 <rajim.beeee@gmail.com>
2021-08-28 15:41:06 +0530

    typed & Untyped constant

---
+++ Constant/constant.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const e int = 12
+const f float64 = 13.45
+const g bool = true
+const h string = "sundar"
+
+const (
+	a = 13
+	b = 14.5
+	c = true
+	d = "muni"
+)
+
+func main() {
+	fmt.Println(a, b, c, d, e, f, g, h)
+
+}
