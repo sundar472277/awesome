1fe085f1a0e7f6402012a9ec2e2af97db3b074ce
RajiMunichamy1993 <rajim.beeee@gmail.com>
2021-08-31 13:26:43 +0530

    Sort.slice

---
+++ Bcrypt/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	//git clone https://github.com/golang/crypto.git
+	//create a folder golang.org/x/crypto/ under GOPATH/src
+	//use this git clone under GOPATH/src/golang.org/x/crypto/
+	s := `password123`
+	fmt.Println()
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), 2)
+	fmt.Println("bs value is :", bs)
+	fmt.Println("error is :", err)
+	if err != nil {
+		fmt.Println("error is :", err)
+	}
+
+	err1 := bcrypt.CompareHashAndPassword(bs, []byte(s))
+	if err != nil {
+		fmt.Println("login failed", err1)
+	} else {
+		fmt.Println("login success")
+	}
+
+}
